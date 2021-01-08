b72e7ebda03e5828ffaae7559fd2423fbaaecdd8
radhika20bansal <radhika.bansal_cs18@gla.ac.in>
2021-01-09 00:42:13 +0530

    commit

---
+++ index.js
@@ -0,0 +1,12 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    dots: true,
+    autoplay: false,
+    responsive: {
+        0: {
+            items: 1
+        }
+    }
+})
