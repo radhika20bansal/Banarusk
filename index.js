27f8566e334226f317bda0e82de1131b59e4aee6
radhika20bansal <radhika.bansal_cs18@gla.ac.in>
2021-02-17 01:43:05 +0530

    menu page

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Navigation Bar
 window.addEventListener("scroll", function() {
     var nav = document.querySelector("nav");
-    nav.classList.toggle("sticky", window.scrollY > 0);
+    nav.classList.toggle("sticky", window.scrollY > 150);
 })
 
 
